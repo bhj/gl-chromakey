c26fd1d8b0990640f26255b5825dd24e4d666a57
Brandon Jones <brandon@radroot.com>
2020-07-09 22:42:44 -0400

    Move shaders to their own files

--- chromagl.js
+++ chromagl.js
@@ -1,147 +1,7 @@
+import fragmentShaderAlphaSrc from './shaders/fragmentShaderAlpha'
+import fragmentShaderPaintSrc from './shaders/fragmentShaderPaint'
+import vertexShaderSrc from './shaders/vertexShader'
 let keyCount = 0
-const vertexShader = `
-  #ifdef GL_ES
-    precision highp float;
-  #endif
-
-  attribute vec3 position;
-  attribute vec2 texCoord;
-
-  varying vec2 vTexCoord;
-  varying vec4 vPosition;
-  varying vec2 vSourceCoord;
-  varying vec2 vAlphaCoord;
-
-  uniform vec4 sourceArea;
-  uniform vec4 alphaArea;
-
-  void main(void) {
-    gl_Position = vec4(position, 1.0);
-    vTexCoord = vec2(texCoord.s, texCoord.t);
-    vSourceCoord = vec2(sourceArea.x + texCoord.s * sourceArea.z, sourceArea.y + texCoord.t * sourceArea.w);
-    vAlphaCoord = vec2(alphaArea.x + texCoord.s * alphaArea.z, alphaArea.y + texCoord.t * alphaArea.w);
-  }
-`
-const fragmentShaderAlpha = `
-  #ifdef GL_ES
-    precision highp float;
-  #endif
-
-  varying vec2 vTexCoord;
-  varying vec4 vPosition;
-  varying vec2 vSourceCoord;
-  varying vec2 vAlphaCoord;
-
-  uniform sampler2D source;
-
-  vec4 sourcePixel;
-  vec4 alphaPixel;
-
-  const mat3 yuv = mat3(
-    54.213, 182.376, 18.411,
-    -54.213, -182.376, 236.589,
-    200.787, -182.376, -18.411
-  );
-
-  vec4 preAlpha(int sourceChannel, int targetChannel, vec4 pixel) {
-    float alpha;
-    if (sourceChannel == 0) {
-      alpha = alphaPixel.r;
-    } else if (sourceChannel == 1) {
-      alpha = alphaPixel.g;
-    } else if (sourceChannel == 2) {
-      alpha = alphaPixel.b;
-    } else {
-      alpha = 0.0;
-    }
-
-    vec4 outputPixel = vec4(pixel);
-    if (targetChannel == 0) {
-      outputPixel.r = min(outputPixel.r, alpha);
-    } else if (targetChannel == 1) {
-      outputPixel.g = min(outputPixel.g, alpha);
-    } else if (targetChannel == 2) {
-      outputPixel.b = min(outputPixel.b, alpha);
-    }
-    return outputPixel;
-  }
-
-  vec4 distAlpha(int targetChannel, vec3 target, float threshold, float fuzzy, vec4 pixel) {
-    float distance2, sum, alpha;
-
-    vec3 yuvColorDiff = sourcePixel.rgb * yuv - target;
-
-    distance2 = dot(yuvColorDiff, yuvColorDiff);
-
-    alpha = smoothstep(threshold, threshold * fuzzy, distance2);
-
-    vec4 outputPixel = vec4(pixel);
-    if (targetChannel == 0) {
-      outputPixel.r *= alpha;
-    } else if (targetChannel == 1) {
-      outputPixel.g *= alpha;
-    } else if (targetChannel == 2) {
-      outputPixel.b *= alpha;
-    }
-    //outputPixel = vec4(abs(x1)/255.0, abs(y1)/255.0, abs(z1)/255.0, 1.0);
-    //outputPixel = sourcePixel;
-    //outputPixel = vec4(target/255.0, 1.0);
-    //outputPixel = vec4(distance2/10000.0, distance2/10000.0, distance2/10000.0, 1.0);
-    return outputPixel;
-  }
-
-  void main(void) {
-    #ifdef pre
-        sourcePixel = texture2D(source, vSourceCoord);
-    #else
-        sourcePixel = texture2D(source, vTexCoord);
-    #endif
-
-    alphaPixel = texture2D(source, vAlphaCoord);
-    vec4 pixel = vec4(1.0);
-    %keys%
-    pixel.a = min(pixel.r, min(pixel.g, pixel.b));
-    gl_FragColor = pixel;
-    //gl_FragColor = alphaPixel;
-    //gl_FragColor = texture2D(source, vTexCoord);
-    //gl_FragColor = vec4(vTexCoord.y, vTexCoord.y, vTexCoord.y, 1.0);
-    //gl_FragColor = vec4(vAlphaCoord.y, vAlphaCoord.y, vAlphaCoord.y, 1.0);
-    //gl_FragColor = vec4(vSourceCoord.y, vSourceCoord.y, vSourceCoord.y, 1.0);
-  }
-`
-const fragmentShaderPaint = `
-  #ifdef GL_ES
-    precision highp float;
-  #endif
-
-  varying vec2 vTexCoord;
-  varying vec4 vPosition;
-  varying vec2 vSourceCoord;
-  varying vec2 vAlphaCoord;
-
-  uniform sampler2D source;
-  uniform sampler2D alpha;
-  uniform vec4 alphaChannel;
-
-  void main(void) {
-    vec4 pixel;
-    vec4 alphaPixel;
-    #ifdef pre
-        pixel = texture2D(source, vSourceCoord);
-    #else
-        pixel = texture2D(source, vTexCoord);
-    #endif
-    alphaPixel = texture2D(alpha, vec2(vTexCoord.x, 1.0 - vTexCoord.y));
-    /*
-    set this vector because a dot product should be MUCH faster
-    in a shader than a big "if" statement        */
-    pixel.a = dot(alphaPixel, alphaChannel);
-    gl_FragColor = pixel;
-    //gl_FragColor = alphaPixel;
-    //gl_FragColor = vec4(alphaPixel.r, vTexCoord.y, vTexCoord.y, 1.0);
-    //gl_FragColor = vec4(alphaPixel.r, vAlphaCoord.y, vAlphaCoord.y, 1.0);
-  }
-`
 
 function checkType (object, type) {
   return Object.prototype.toString.call(object) === `[object ${type}]`
@@ -256,11 +116,11 @@ function setUpShaders () {
     keyFunctions = 'pixel = sourcePixel;\n'
   }
 
-  let fragmentSrc = fragmentShaderAlpha.replace('%keys%', keyFunctions)
+  let fragmentSrc = fragmentShaderAlphaSrc.replace('%keys%', keyFunctions)
   if (hasPreCalc) {
     fragmentSrc = `#define pre\n${fragmentSrc}`
   }
-  this.alphaShader = new ShaderProgram(gl, vertexShader, fragmentSrc)
+  this.alphaShader = new ShaderProgram(gl, vertexShaderSrc, fragmentSrc)
 
   const sourceX = this.sourceX
   const sourceY = this.sourceY
@@ -275,11 +135,11 @@ function setUpShaders () {
   this.alphaShader.set_sourceArea(sourceX, sourceY, sourceWidth, sourceHeight)
   this.alphaShader.set_alphaArea(alphaX, alphaY, alphaWidth, alphaHeight)
 
-  let painterSrc = fragmentShaderPaint
+  let painterSrc = fragmentShaderPaintSrc
   if (hasPreCalc) {
     painterSrc = `#define pre\n${painterSrc}`
   }
-  this.paintShader = new ShaderProgram(gl, vertexShader, painterSrc)
+  this.paintShader = new ShaderProgram(gl, vertexShaderSrc, painterSrc)
   this.paintShader.set_sourceArea(sourceX, sourceY, sourceWidth, sourceHeight)
   this.paintShader.set_alphaArea(alphaX, alphaY, alphaWidth, alphaHeight)
 }
